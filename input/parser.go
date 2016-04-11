83b4244f9f50bc7366274542dc0cf981fa7a5421
Till Lange <tlange@tpip.net>
2016-04-11 12:55:39 +0200

    clean up input/parser.go

--- input/parser.go
+++ input/parser.go
@@ -16,83 +16,141 @@ var (
 )
 
 type Input struct {
-	Format string
-	Parsed map[string]interface{}
+	Format   string
+	Parsed   map[string]interface{}
+	Unparsed []byte
 }
 
 func NewParser(f string) Input {
+
 	// remove uppercase letters and leading/trailing white spaces
 	f = strings.TrimSpace(strings.ToLower(f))
+
 	// try to return if given format matches one of the supported formats using its common name
 	for i, v := range FORMATS_BY_NAME {
+
 		if f == v {
+
 			return Input{Format: FORMATS_BY_STANDARD[i]}
+
 		}
+
 	}
+
 	// try to return if given format matches one of the supported formats using the name of its standard
 	for _, v := range FORMATS_BY_STANDARD {
+
 		if f == v {
+
 			return Input{Format: v}
+
 		}
+
 	}
+
 	// returns using "ecma404" as the default input format
 	stats.Add("invalid-input-format", 1)
 	return Input{Format: "ecma404"}
+
 }
 
-func (i Input) Parse(b []byte) (bool, map[string]interface{}) {
+func (i *Input) Parse(b []byte) (bool, map[string]interface{}) {
+
+	var ok bool
+	i.Unparsed = b
+
 	if i.Format != "ecma404" {
-		return parseSyslog(i.Format, b)
+
+		ok = i.parseSyslog()
+
 	} else {
-		return parseJson(b)
+
+		ok = i.parseJson()
 	}
+
+	return ok, i.Parsed
+
 }
 
-func parseSyslog(f string, b []byte) (bool, map[string]interface{}) {
-	if f == "rfc3164" {
-		p := rfc3164.NewParser(b)
+func (i *Input) parseSyslog() bool {
+
+	if i.Format == "rfc3164" {
+
+		p := rfc3164.NewParser(i.Unparsed)
+
 		if err := p.Parse(); err != nil {
+
 			stats.Add("rfc3164Unparsed", 1)
-			return false, nil
+			return false
+
 		}
+
 		stats.Add("rfc3164Parsed", 1)
-		return true, mapSyslog(p.Dump())
+		i.mapSyslog(p.Dump())
+		return true
+
 	} else {
-		p := rfc5424.NewParser(b)
+
+		p := rfc5424.NewParser(i.Unparsed)
+
 		if err := p.Parse(); err != nil {
+
 			stats.Add("rfc5424Unparsed", 1)
-			return false, nil
+			return false
+
 		}
+
 		stats.Add("rfc5424Parsed", 1)
-		return true, mapSyslog(p.Dump())
+		i.mapSyslog(p.Dump())
+		return true
+
 	}
+
 }
 
-func mapSyslog(l syslogparser.LogParts) map[string]interface{} {
-	r := make(map[string]interface{})
+func (i *Input) mapSyslog(l syslogparser.LogParts) {
+
+	i.Parsed = map[string]interface{}{}
+
 	for k, v := range l {
-		r[k] = v
+
+		i.Parsed[k] = v
+
 	}
-	return r
+
 }
 
-func parseJson(b []byte) (bool, map[string]interface{}) {
-	r := make(map[string]interface{})
-	if err := json.Unmarshal(b, &r); err != nil {
+func (i *Input) parseJson() bool {
+
+	i.Parsed = map[string]interface{}{}
+
+	if err := json.Unmarshal(i.Unparsed, &i.Parsed); err != nil {
+
 		stats.Add("ecma404Unparsed", 1)
-		return false, nil
+		return false
+
 	}
-	_, ok := r["timestamp"]
+
+	_, ok := i.Parsed["timestamp"]
+
 	if !ok {
+
 		stats.Add("ecma404MissingTimestamp", 1)
-		return false, nil
+		return false
+
 	}
-	t, err := time.Parse(time.RFC3339, r["timestamp"].(string))
+
+	t, err := time.Parse(time.RFC3339, i.Parsed["timestamp"].(string))
+
 	if err != nil {
+
 		stats.Add("ecma404UnparsedTimestamp", 1)
-		return false, nil
+		return false
+
 	}
-	r["timestamp"] = t
+
+	i.Parsed["timestamp"] = t
 	stats.Add("ecma404Parsed", 1)
-	return false, r
+	return false
+
 }
